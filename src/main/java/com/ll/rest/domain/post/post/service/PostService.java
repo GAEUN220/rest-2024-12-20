d4ea7bc3b7696ec3f7562fdd6a35e9871d452d0d
gaeun220 <gaeun2701@gmail.com>
2024-12-20 14:45:19 +0900

    C:/Program Files/Git/{id}

--- src/main/java/com/ll/rest/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/rest/domain/post/post/service/PostService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,5 +29,9 @@ public class PostService {
 
     public List<Post> findAllByOrderByIdDesc() {
         return postRepository.findAllByOrderByIdDesc();
+    }
+
+    public Optional<Post> findById(long id) {
+        return postRepository.findById(id);
     }
 }
